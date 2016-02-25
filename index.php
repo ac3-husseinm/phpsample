246ad37080b2f4f0df522bf9c3d54ea1d4360aca
Hussein Muslmani <hussein.muslmani@ac3.com.au>
2016-02-25 14:33:58 +1100

    fisherman

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 <h2>Hello World! </h2><br>
-<img src="http://www.ac3.com.au/emailsigs/v2/ac3_logo.png">
+<img src="http://www.ac3.com.au/emailsigs/v2/avatar000077.png">
 </body>
 </html>
 
